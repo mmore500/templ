c3c97d5bd79e0e276bb7bcac985dff467f9652f5
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-01-22 00:18:14 -0500

    Bump version: 0.6.2 → 0.6.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     entry_points = {
         "console_scripts": ['templ = templ.templ:main']
         },
-    version = "0.6.2",
+    version = "0.6.3",
     description = "A templating tool for maintaining a journal or notes.",
     long_description = long_descr,
     author = "Matthew Andres Moreno",
